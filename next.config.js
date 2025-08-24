bed79d5696ebf1ab73cbe4f33f9f8da8ff720b3e
noshadramdani <noshadprofessional69@gmail.com>
2025-08-24 09:07:09 +0500

    Code updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 const path = require('path')
 
-const repoName = 'my_portfolio_react' // 🔴 Replace with your GitHub repo name
-
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  // output: 'export',
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true,
     remotePatterns: [
